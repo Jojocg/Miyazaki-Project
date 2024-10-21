5f40e9dfed25c15e66fea6c92e7122b52cfcc615
ofeliatp <ofetadeo86@gmail.com>
2024-10-21 13:16:44 +0100

    cambios antes descanso 13h

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function createForm() {
     const news = document.getElementById("form-div");
     const firstnameDiv = document.getElementById("firstname-div");
@@ -6,7 +5,9 @@ function createForm() {
     const checkboxDiv = document.getElementById("checkbox-div");
     const buttonDiv = document.getElementById("button-div");
     const form = document.createElement("form");
-    form.setAttribute('method', 'post');
+    /* form.setAttribute('method', 'post'); */
+    form.setAttribute("id", "formStyle")
+    form.setAttribute("onsubmit", "event.preventDefault();")
 
     const labelName = document.createElement("label");
     labelName.setAttribute("for", "firstname");
@@ -15,6 +16,7 @@ function createForm() {
     const inputName = document.createElement("input");
     inputName.setAttribute("type", "text");
     inputName.setAttribute("id", "firstname");
+    inputName.setAttribute("class", "inputForm");
 
     const labelEmail = document.createElement("label");
     labelEmail.setAttribute("for", "email");
@@ -23,10 +25,12 @@ function createForm() {
     const inputEmail = document.createElement("input");
     inputEmail.setAttribute("type", "email");
     inputEmail.setAttribute("id", "email");
+    inputEmail.setAttribute("class", "inputForm");
 
     const checkBox = document.createElement("input");
     checkBox.setAttribute("type", "checkbox");
     checkBox.setAttribute("id", "agree");
+
     const labelCheckbox = document.createElement("label");
     labelCheckbox.innerHTML = "<label>Acepto los <a href=''>términos y condiciones</a></label>";
     labelCheckbox.setAttribute("for", "agree");
@@ -44,7 +48,20 @@ function createForm() {
     checkboxDiv.appendChild(checkBox);
     checkboxDiv.appendChild(labelCheckbox);
     buttonDiv.appendChild(buttonSubmit);
+    form.appendChild(firstnameDiv);
+    form.appendChild(emailDiv);
+    form.appendChild(checkboxDiv);
+    form.appendChild(buttonDiv);
     news.appendChild(form);
+
+    function resetInput() {
+        const inputsForm = document.getElementsByClassName("inputForm");
+        for (let i = 0; i < inputsForm.length; i++) {
+            inputsForm[i].value = "";
+        }
+        checkBox.checked = false;
+    }
+    buttonSubmit.addEventListener("click", resetInput);
 }
 createForm();
 
@@ -68,6 +85,18 @@ function cookiesWindow() {
     
 }
 cookiesWindow(); 
+
+
+
+function activate(e) {
+    const slider = document.querySelector(".slider");
+    const items = document.querySelectorAll(".item_carrousel");
+    e.target.matches(".next") && slider.append(items[0]);
+    e.target.matches(".prev") && slider.prepend(items[items.length - 1]);
+}
+
+document.addEventListener("click", activate, false);
+
 
 
 
